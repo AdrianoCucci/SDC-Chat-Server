59d533c89d9f50c2acfe48a6b88c266f906fca12
Adriano Cucci <cucciadriano@gmail.com>
2021-08-29 19:41:11 -0400

    Fixed bugs.

--- src/database/entity-change-tracker.ts
+++ src/database/entity-change-tracker.ts
@@ -6,46 +6,46 @@ export class EntityChangeTracker<T> {
   public readonly deletions = new Map<T, number>();
 
   public setAdd(entity: T): void {
-    const existingState: EntityTrackState = this.getTrackedState(entity);
-
-    if(existingState === EntityTrackState.Update) {
-      this.updates.delete(entity);
-    }
-    else if(existingState === EntityTrackState.Delete) {
-      this.deletions.delete(entity);
-    }
-
     if(!this.additions.includes(entity)) {
+      const existingState: EntityTrackState = this.getTrackedState(entity);
+
+      if(existingState === EntityTrackState.Update) {
+        this.updates.delete(entity);
+      }
+      else if(existingState === EntityTrackState.Delete) {
+        this.deletions.delete(entity);
+      }
+
       this.additions.push(entity);
     }
   }
 
   public setUpdate(entity: T, updateID: number): void {
-    const existingState: EntityTrackState = this.getTrackedState(entity);
-
-    if(existingState === EntityTrackState.Add) {
-      this.additions.splice(this.additions.indexOf(entity), 1);
-    }
-    else if(existingState === EntityTrackState.Delete) {
-      this.deletions.delete(entity);
-    }
-
     if(!this.updates.has(entity)) {
+      const existingState: EntityTrackState = this.getTrackedState(entity);
+
+      if(existingState === EntityTrackState.Add) {
+        this.additions.splice(this.additions.indexOf(entity), 1);
+      }
+      else if(existingState === EntityTrackState.Delete) {
+        this.deletions.delete(entity);
+      }
+
       this.updates.set(entity, updateID);
     }
   }
 
   public setDelete(entity: T, deleteID: number): void {
-    const existingState: EntityTrackState = this.getTrackedState(entity);
-
-    if(existingState === EntityTrackState.Add) {
-      this.additions.splice(this.additions.indexOf(entity), 1);
-    }
-    if(existingState === EntityTrackState.Update) {
-      this.updates.delete(entity);
-    }
-
     if(!this.deletions.has(entity)) {
+      const existingState: EntityTrackState = this.getTrackedState(entity);
+
+      if(existingState === EntityTrackState.Add) {
+        this.additions.splice(this.additions.indexOf(entity), 1);
+      }
+      if(existingState === EntityTrackState.Update) {
+        this.updates.delete(entity);
+      }
+
       this.deletions.set(entity, deleteID);
     }
   }
@@ -59,13 +59,13 @@ export class EntityChangeTracker<T> {
   private getTrackedState(entity: T): EntityTrackState {
     let state: EntityTrackState;
 
-    if(this.additions?.includes(entity)) {
+    if(this.additions.includes(entity)) {
       state = EntityTrackState.Add;
     }
-    else if(this.updates?.has(entity)) {
+    else if(this.updates.has(entity)) {
       state = EntityTrackState.Update;
     }
-    else if(this.deletions?.has(entity)) {
+    else if(this.deletions.has(entity)) {
       state = EntityTrackState.Delete;
     }
     else {
