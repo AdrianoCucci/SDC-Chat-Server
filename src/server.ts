b8410c4d1bd5cd82408a69d9d4519bed66acfb0a
Adriano Cucci <cucciadriano@gmail.com>
2021-08-29 15:37:16 -0400

    Implemented CORS options.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import express, { Application } from "express";
 import { Server as HttpServer, createServer } from "http";
-import { Server as SocketServer } from "socket.io";
+import { Server as SocketServer, ServerOptions, Socket } from "socket.io";
 
 const expressApp: Application = express();
 const httpServer: HttpServer = createServer(expressApp);
-const socketServer: SocketServer = new SocketServer(httpServer);
 
-socketServer.on("connection", () => console.log("New web socket connection..."));
+const socketServerOptions: Partial<ServerOptions> = {
+  cors: {
+    origin: "http://localhost:4200",
+    allowedHeaders: ["GET", "POST"]
+  }
+}
+
+const socketServer = new SocketServer(httpServer, socketServerOptions);
+
+socketServer.on("connection", (socket: Socket) => {
+  console.log("New web socket connection...");
+
+  socket.on("disconnect", (reason: string) => {
+    console.log("Socket disconnected...\nReason: ", reason);
+  });
+});
+
 
 const port: string | number = process.env.PORT || 3000;
 httpServer.listen(port, () => console.log(`Server running on port: ${port}`));
