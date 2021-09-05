40251b6bf7d47ecf9533602a333bea86bf7f93c5
Adriano Cucci <cucciadriano@gmail.com>
2021-09-04 22:23:03 -0400

    Configured dotenv.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express, { Application, json } from "express";
+import { config as dotEnvConfig } from "dotenv";
 import { Server as HttpServer, createServer } from "http";
 import { Server as SocketServer } from "socket.io";
 import { InMemoryDbContext } from "./database/in-memory-db-context";
@@ -7,6 +8,8 @@ import { IDbContext } from "./database/interfaces/db-context";
 import { ApiService } from "./services/api-service";
 import { MapperService } from "./services/mapper-service";
 import { SocketService } from "./services/socket-service";
+
+dotEnvConfig();
 
 const corsOptions = {
   origin: "http://localhost:4200",
