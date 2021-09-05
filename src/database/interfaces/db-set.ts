5500c07da0534535b507fb3ebd9ba289e8e24ed0
Adriano Cucci <cucciadriano@gmail.com>
2021-09-04 22:52:21 -0400

    Extended IDbSet functions.

--- src/database/interfaces/db-set.ts
+++ src/database/interfaces/db-set.ts
@@ -5,6 +5,10 @@ export interface IDbSet<T> {
 
   getById(entityId: number): Promise<T>;
 
+  getFirstWhere(predicate: (entity: T) => boolean): Promise<T>;
+
+  getAllWhere(predicate: (entity: T) => boolean): Promise<T[]>;
+
   add(entity: T): void;
 
   update(entityId: number, newEntity: T): void;
