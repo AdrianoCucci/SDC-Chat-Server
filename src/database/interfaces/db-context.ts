192b1f2da82e51cffd8501eb540ff2f285e9a105
Adriano Cucci <cucciadriano@gmail.com>
2021-09-08 18:10:24 -0400

    Updated DB context.

--- src/database/interfaces/db-context.ts
+++ src/database/interfaces/db-context.ts
@@ -1,10 +1,14 @@
 import { ChatMessage } from "../../models/messages/chat-message";
 import { UserMessage } from "../../models/messages/user-message";
+import { Organization } from "../../models/organizations/organization";
+import { Room } from "../../models/rooms/room";
 import { User } from "../../models/users/user";
 import { IDbSet } from "./db-set";
 
 export interface IDbContext {
   users: IDbSet<User>;
+  organizations: IDbSet<Organization>;
+  rooms: IDbSet<Room>;
   messages: IDbSet<ChatMessage>;
   usersMessages: IDbSet<UserMessage>;
 }
