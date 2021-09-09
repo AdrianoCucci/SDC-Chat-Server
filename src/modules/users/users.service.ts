a4464bfe9ece4b1c79ef620cb4c1192c69d456e5
Adriano Cucci <cucciadriano@gmail.com>
2021-09-09 17:35:16 -0400

    Updated UsersService.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { Role } from 'src/models/auth/role';
 import { User } from 'src/models/users/user';
 
@@ -22,20 +22,29 @@ export class UsersService {
   }
 
   public async getById(id: number): Promise<User> {
-    const user: User = this._users.find((u: User) => u.id === id);
+    return this._users.find((u: User) => u.id === id);
+  }
 
-    if(user == null) {
-      throw this.idNotFound(id);
-    }
-
-    return user;
+  public async getByUsername(username: string): Promise<User> {
+    return this._users.find((u: User) => u.username === username);
   }
 
   public async idExists(id: number): Promise<boolean> {
     return this._users.findIndex((u: User) => u.id === id) !== -1;
   }
 
+  public async usernameExists(username: string): Promise<boolean> {
+    return this._users.findIndex((u: User) => u.username === username) !== -1;
+  }
+
   public async add(user: User): Promise<User> {
+    if(!user.password) {
+      throw new BadRequestException("Password is required");
+    }
+    if(await this.usernameExists(user.username)) {
+      throw this.usernameConflictException(user.username);
+    }
+
     user.id = this._nextId;
 
     this._users.push(user);
@@ -46,9 +55,13 @@ export class UsersService {
 
   public async update(id: number, user: User): Promise<User> {
     const existingUser: User = await this.getById(id);
-
     if(existingUser == null) {
-      throw this.idNotFound(id);
+      throw this.idNotFoundException(id);
+    }
+
+    const userByUsername: User = await this.getByUsername(user.username);
+    if(userByUsername != null && userByUsername.id !== id) {
+      throw this.usernameConflictException(user.username);
     }
 
     const index: number = this._users.indexOf(existingUser);
@@ -59,9 +72,8 @@ export class UsersService {
 
   public async delete(id: number): Promise<boolean> {
     const user: User = await this.getById(id);
-
     if(user == null) {
-      throw this.idNotFound(id);
+      throw this.idNotFoundException(id);
     }
 
     const index: number = this._users.indexOf(user);
@@ -70,7 +82,11 @@ export class UsersService {
     return true;
   }
 
-  private idNotFound(id: number): NotFoundException {
+  private idNotFoundException(id: number): NotFoundException {
     return new NotFoundException(`User with ID does not exist: ${id}`);
+  }
+
+  private usernameConflictException(username: string): ConflictException {
+    return new ConflictException(`Username already exists: ${username}`);
   }
 }
