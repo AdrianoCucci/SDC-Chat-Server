c18f5108153dc4d7c47999c5d3c42cfc16ab3093
Adriano Cucci <cucciadriano@gmail.com>
2021-09-09 18:11:59 -0400

    Implemented UsersController.

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { User } from 'src/models/users/user';
+import { UserRequest } from 'src/models/users/user-request';
+import { UserResponse } from 'src/models/users/user-response';
+import { MapperService } from 'src/utils/dto-mappings/mapper.service';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController { }
+export class UsersController {
+  constructor(private _usersService: UsersService, private _mapper: MapperService) { }
+
+  @Get()
+  public async getAllUsers(): Promise<UserResponse[]> {
+    const users: User[] = await this._usersService.getAll();
+    const dtos: UserResponse[] = this._mapper.users.toResponses(users);
+
+    return dtos;
+  }
+
+  @Get(":id")
+  public async getUserById(@Param("id", ParseIntPipe) id: number): Promise<UserResponse> {
+    const user: User = await this._usersService.tryGetById(id);
+    const dto: UserResponse = this._mapper.users.toResponse(user);
+
+    return dto;
+  }
+
+  @Post()
+  public async postUser(@Body() request: UserRequest): Promise<UserResponse> {
+    const user: User = this._mapper.users.toEntity(request);
+    await this._usersService.add(user);
+
+    const dto: UserResponse = this._mapper.users.toResponse(user);
+    return dto;
+  }
+
+  @Put(":id")
+  public async putUser(@Param("id", ParseIntPipe) id: number, @Body() request: UserRequest): Promise<UserResponse> {
+    let user: User = this._mapper.users.toEntity(request);
+    user = await this._usersService.update(id, user);
+
+    const dto: UserResponse = this._mapper.users.toResponse(user);
+    return dto;
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async deleteUser(@Param("id", ParseIntPipe) id: number): Promise<void> {
+    await this._usersService.delete(id);
+  }
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MapperService } from 'src/utils/dto-mappings/mapper.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, MapperService]
 })
 export class UsersModule { }
