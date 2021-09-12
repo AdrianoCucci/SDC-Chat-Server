6697ebeb521b443d86467fd99e0674ec55363e64
Adriano Cucci <cucciadriano@gmail.com>
2021-09-12 19:00:35 -0400

    Fully implemented AppWebSocketGateway

--- src/modules/app-web-socket/app-web-socket.gateway.ts
+++ src/modules/app-web-socket/app-web-socket.gateway.ts
@@ -1,5 +1,6 @@
 import { OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { ChatMessage } from 'src/models/chat-messages/chat-message';
 import { User } from 'src/models/users/user';
 import { UserResponse } from 'src/models/users/user-response';
 import { MapperService } from 'src/utils/dto-mappings/mapper.service';
@@ -16,51 +17,45 @@ export class AppWebSocketGateway implements OnGatewayDisconnect {
 
   constructor(private _messagesService: ChatMessagesService, private _usersService: UsersService, private _mapper: MapperService) { }
 
-  public async handleDisconnect(socket: Socket): Promise<void> {
+  public handleDisconnect(socket: Socket): void {
     const user: UserResponse = this._socketUserMap.get(socket);
 
     if(user != null) {
-      const userId: number = user.id;
       user.isOnline = false;
-
-      console.log("USER LEAVE", user);
       socket.broadcast.emit(SOCKET_EVENTS.userLeave, user);
 
+      this.updateUserOnline(user.id, false);
       this._socketUserMap.delete(socket);
-      await this.updateDisconnectedUser(userId);
     }
   }
 
   @SubscribeMessage(SOCKET_EVENTS.userJoin)
-  public async onUserJoin(socket: Socket, payload: UserResponse): Promise<void> {
-    payload.isOnline = true;
-
+  public onUserJoin(socket: Socket, payload: UserResponse): void {
     this._socketUserMap.set(socket, payload);
-    console.log("USER JOIN", payload);
+
+    payload.isOnline = true;
     socket.broadcast.emit(SOCKET_EVENTS.userJoin, payload);
 
-    await this.updateConnectedUser(payload.id);
+    this.updateUserOnline(payload.id, true);
   }
 
-  private async updateConnectedUser(userId: number): Promise<void> {
-    const userEntity: User = await this._usersService.getById(userId);
+  @SubscribeMessage(SOCKET_EVENTS.message)
+  public async onMessage(socket: Socket, payload: ChatMessage): Promise<void> {
+    if(payload != null && payload.contents && payload.senderUserId != null) {
+      socket.broadcast.emit(SOCKET_EVENTS.message, payload);
 
-    if(userEntity != null) {
-      userEntity.isOnline = true;
-      await this._usersService.update(userEntity);
-
-      console.log("UPDATED USER ENTITY");
+      if(await this._usersService.idExists(payload.senderUserId)) {
+        await this._messagesService.add(payload);
+      }
     }
   }
 
-  private async updateDisconnectedUser(userId: number): Promise<void> {
+  private async updateUserOnline(userId: number, isOnline: boolean): Promise<void> {
     const userEntity: User = await this._usersService.getById(userId);
 
     if(userEntity != null) {
-      userEntity.isOnline = false;
+      userEntity.isOnline = isOnline;
       await this._usersService.update(userEntity);
-
-      console.log("UPDATED USER ENTITY");
     }
   }
 }
