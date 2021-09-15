0fe91da5494cf8dd2cd04637409518eba0f4cdd7
Adriano Cucci <cucciadriano@gmail.com>
2021-09-15 16:15:04 -0400

    Implemented OrganizationsService.

--- src/modules/organizations/organizations.service.ts
+++ src/modules/organizations/organizations.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Organization } from 'src/models/organizations/organization';
+import { ServiceBase } from 'src/utils/service-base';
 
 @Injectable()
-export class OrganizationsService { }
+export class OrganizationsService extends ServiceBase<Organization> {
+  constructor() {
+    super("id",
+      [
+        {
+          id: 1,
+          name: "Sorriso Dental Care",
+          email: "contact@sdc.ca",
+          phoneNumber: "555-555-5555",
+          street: "123 Test St.",
+          city: "Toronto",
+          province: "Ontario",
+          country: "Canada",
+          postalCode: "L7B 2G9"
+        }
+      ]
+    );
+  }
+}
