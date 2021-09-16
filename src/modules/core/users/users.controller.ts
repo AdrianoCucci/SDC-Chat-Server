757a8438799604f1c43a3ec92d2600c06e312aac
Adriano Cucci <cucciadriano@gmail.com>
2021-09-15 23:28:00 -0400

    Updated UsersController to allow OrganizationAdmins to add/update/delete users for their Organizations.

--- src/modules/core/users/users.controller.ts
+++ src/modules/core/users/users.controller.ts
@@ -33,18 +33,19 @@ export class UsersController {
   }
 
   @Post()
-  @Roles(Role.Administrator)
-  public async postUser(@Body() request: UserDto): Promise<UserDto> {
-    await this.validatePostModel(request);
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
+  public async postUser(@RequestUser() user: UserDto, @Body() request: UserDto): Promise<UserDto> {
+    await this.validatePostModel(user, request);
 
-    const user: User = this._mapper.users.mapEntity(request);
-    await this._usersService.add(user);
+    const entity: User = this._mapper.users.mapEntity(request);
+    await this._usersService.add(entity);
 
-    const dto: UserDto = this._mapper.users.mapDto(user);
+    const dto: UserDto = this._mapper.users.mapDto(entity);
     return dto;
   }
 
   @Put(":id")
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
   public async putUser(@RequestUser() user: UserDto, @Param("id", ParseIntPipe) id: number, @Body() request: UserDtoPartial): Promise<UserDto> {
     const userEntity: User = await this.tryGetUserById(id);
 
@@ -67,13 +68,16 @@ export class UsersController {
   }
 
   @Delete(":id")
-  @Roles(Role.Administrator)
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
   @HttpCode(HttpStatus.NO_CONTENT)
   public async deleteUser(@RequestUser() user: UserDto, @Param("id", ParseIntPipe) id: number): Promise<void> {
     const entity: User = await this.tryGetUserById(id);
 
     if(user.id === entity.id) {
       throw new ForbiddenException("You may not delete your own user account");
+    }
+    else if(user.role !== Role.Administrator && user.organizationId !== entity.organizationId) {
+      throw new ForbiddenException("You do not have permission to delete this user");
     }
 
     await this._usersService.delete(id);
@@ -89,14 +93,24 @@ export class UsersController {
     return user;
   }
 
-  private async validatePostModel(request: UserDto): Promise<void> {
+  private async validatePostModel(requestUser: UserDto, request: UserDto): Promise<void> {
     const errors: string[] = [];
 
+    //Make sure non-administrators can only add users within their own organization.
+    if(requestUser.role !== Role.Administrator) {
+      request.organizationId = requestUser.organizationId;
+      request.role = Role.User;
+    }
     if(await this._usersService.usernameExists(request.username)) {
       errors.push(`username already exists: ${request.username}`);
     }
-    if(request.organizationId != null && !await this._orgService.idExists(request.organizationId)) {
-      errors.push(`organizationId does not exist: ${request.username}`);
+
+    //Non-administrators must specify the organization of users.
+    if(request.organizationId == null && requestUser.role !== Role.Administrator) {
+      errors.push("organizationId is required");
+    }
+    else if(request.organizationId != null && !await this._orgService.idExists(request.organizationId)) {
+      errors.push(`organizationId does not exist: ${request.organizationId}`);
     }
 
     if(errors.length > 0) {
@@ -107,6 +121,11 @@ export class UsersController {
   private async validatePutModel(requestUser: UserDto, entity: User, request: UserDtoPartial): Promise<void> {
     const errors: string[] = [];
 
+    //Make sure non-administrators can only update users within their own organization.
+    if(requestUser.role !== Role.Administrator) {
+      request.organizationId = requestUser.organizationId;
+      request.role = Role.User;
+    }
     if(request.username) {
       const userByUsername: User = await this._usersService.getByUsername(request.username);
 
@@ -114,9 +133,15 @@ export class UsersController {
         errors.push(`username already exists: ${request.username}`);
       }
     }
-    if(request.organizationId != null && !await this._orgService.idExists(request.organizationId)) {
-      errors.push(`organizationId does not exist: ${request.username}`);
+    
+    //Non-administrators must specify the organization of users.
+    if(request.organizationId == null && requestUser.role !== Role.Administrator) {
+      errors.push("organizationId is required");
     }
+    else if(request.organizationId != null && !await this._orgService.idExists(request.organizationId)) {
+      errors.push(`organizationId does not exist: ${request.organizationId}`);
+    }
+
     if(requestUser.id !== entity.id && requestUser.role !== Role.Administrator) {
       errors.push("You may not edit a different user's information");
     }
