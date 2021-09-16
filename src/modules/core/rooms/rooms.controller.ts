c349e40cfdbe2bbec9caa8c8102a8dace586910d
Adriano Cucci <cucciadriano@gmail.com>
2021-09-15 23:02:13 -0400

    Implemented RoomsController.

--- src/modules/core/rooms/rooms.controller.ts
+++ src/modules/core/rooms/rooms.controller.ts
@@ -1,4 +1,95 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param, ParseIntPipe, NotFoundException, Post, Body, ConflictException, Put, ForbiddenException, Delete, HttpCode, HttpStatus, UseGuards } from "@nestjs/common";
+import { RequestUser } from "src/decorators/request-user.decorator";
+import { Roles } from "src/decorators/roles.decorator";
+import { Role } from "src/models/auth/role";
+import { Room } from "src/models/rooms/room";
+import { RoomDto } from "src/models/rooms/room-dto";
+import { RoomDtoPartial } from "src/models/rooms/room-dto-partial";
+import { RoomParams } from "src/models/rooms/room-params";
+import { UserDto } from "src/models/users/user-dto";
+import { AuthorizeGuard } from "src/modules/shared/jwt-auth/authorize.guard";
+import { MapperService } from "src/modules/shared/mapper/mapper.service";
+import { OrganizationsService } from "../organizations/organizations.service";
+import { RoomsService } from "./rooms.service";
 
 @Controller("api/rooms")
-export class RoomsController { }
+@UseGuards(AuthorizeGuard)
+export class RoomsController {
+  constructor(private _roomsService: RoomsService, private _orgsService: OrganizationsService, private _mapper: MapperService) { }
+
+  @Get()
+  public async getAllRooms(@Query() params: RoomParams): Promise<RoomDto[]> {
+    const rooms: Room[] = await this._roomsService.getAll(params);
+    const dtos: RoomDto[] = this._mapper.rooms.mapDtos(rooms);
+
+    return dtos;
+  }
+
+  @Get(":id")
+  public async getRoomById(@Param("id", ParseIntPipe) id: number): Promise<RoomDto> {
+    const room: Room = await this.tryGetRoomById(id);
+    const dto: RoomDto = this._mapper.rooms.mapDto(room);
+
+    return dto;
+  }
+
+  @Post()
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
+  public async postRoom(@Body() request: RoomDto): Promise<RoomDto> {
+    await this.validateRequest(request);
+
+    const room: Room = this._mapper.rooms.mapEntity(request);
+    await this._roomsService.add(room);
+
+    const response: RoomDto = this._mapper.rooms.mapDto(room);
+    return response;
+  }
+
+  @Put(":id")
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
+  public async putRoom(@RequestUser() user: UserDto, @Param("id", ParseIntPipe) id: number, @Body() request: RoomDtoPartial): Promise<RoomDto> {
+    const room: Room = await this.tryGetRoomById(id);
+
+    if(user.organizationId !== room.organizationId && user.role !== Role.Administrator) {
+      throw new ForbiddenException("You do not have permission to update this room");
+    }
+    if(request.organizationId != null) {
+      await this.validateRequest(request);
+    }
+
+    this._mapper.rooms.mapEntity(request, room);
+    await this._roomsService.update(room);
+
+    const response: RoomDto = this._mapper.rooms.mapDto(room);
+    return response;
+  }
+
+  @Delete(":id")
+  @Roles(Role.Administrator, Role.OrganizationAdmin)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async deleteRoom(@RequestUser() user: UserDto, @Param("id", ParseIntPipe) id: number): Promise<void> {
+    const room: Room = await this.tryGetRoomById(id);
+
+    if(user.organizationId !== room.organizationId && user.role !== Role.Administrator) {
+      throw new ForbiddenException("You do not have permission to delete this room");
+    }
+    
+    await this._roomsService.delete(id);
+  }
+
+  private async tryGetRoomById(id: number): Promise<Room> {
+    const room: Room = await this._roomsService.getById(id);
+
+    if(room == null) {
+      throw new NotFoundException(`Room ID does not exist: ${id}`);
+    }
+
+    return room;
+  }
+
+  private async validateRequest(request: RoomDto | RoomDtoPartial): Promise<void> {
+    if(!await this._orgsService.idExists(request.organizationId)) {
+      throw new ConflictException(`organizationId does not exist: ${request.organizationId}`);
+    }
+  }
+}
