5304960a81f10fa388a6b47e8547fb0278a7d764
Adriano Cucci <cucciadriano@gmail.com>
2021-09-15 22:31:21 -0400

    Implemented RoomsService.

--- src/modules/core/rooms/rooms.service.ts
+++ src/modules/core/rooms/rooms.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Room } from 'src/models/rooms/room';
+import { ServiceBase } from 'src/utils/service-base';
 
 @Injectable()
-export class RoomsService { }
+export class RoomsService extends ServiceBase<Room> {
+  constructor() {
+    super("id",
+      [
+        {
+          id: 1,
+          name: "Operating Room",
+          description: "Sample text description...",
+          number: 261,
+          organizationId: 1
+        }
+      ]
+    );
+  }
+}
