cf0b2122b7fb641a06ad8774f3b40541fae51491
Adriano Cucci <cucciadriano@gmail.com>
2021-11-08 15:21:34 -0500

    Added compareHash function.

--- src/utils/password-utils.ts
+++ src/utils/password-utils.ts
@@ -1,5 +1,7 @@
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcrypt';
 
 export const generateSalt = async (rounds: number = 10): Promise<string> => await genSalt(rounds);
 
 export const computeHash = async (value: string, salt: string): Promise<string> => await hash(value, salt);
+
+export const compareHash = async (plainText: string, hash: string): Promise<boolean> => await compare(plainText, hash);
