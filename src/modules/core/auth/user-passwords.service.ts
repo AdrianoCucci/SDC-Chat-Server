d5d2cd4b4a932e925d8fca1f3468416c9ecc04d9
Adriano Cucci <cucciadriano@gmail.com>
2021-11-08 15:21:55 -0500

    Updated placeholder passwords in UserPasswordsService.

--- src/modules/core/auth/user-passwords.service.ts
+++ src/modules/core/auth/user-passwords.service.ts
@@ -6,11 +6,11 @@ import { ServiceBase } from "src/utils/service-base";
 export class UserPasswordsService extends ServiceBase<UserPassword> {
   constructor() {
     super("id", [
-      new UserPassword({ id: 1, value: "12345", salt: "", userId: 1 }),
-      new UserPassword({ id: 2, value: "12345", salt: "", userId: 2 }),
-      new UserPassword({ id: 3, value: "12345", salt: "", userId: 3 }),
-      new UserPassword({ id: 4, value: "12345", salt: "", userId: 4 }),
-      new UserPassword({ id: 5, value: "12345", salt: "", userId: 5 })
+      new UserPassword({ id: 1, value: "$2b$10$38n8HSG4mnKC6tMnkjW11eYokxw8eOBZ.otQsf8LfC9i7TpVgjh5a", salt: "$2b$10$38n8HSG4mnKC6tMnkjW11e", userId: 1 }),
+      new UserPassword({ id: 2, value: "$2b$10$ShcZ4K9RhFoNIwaz7mt0re/WZpQRyiVpW7SfJTos/4Zmv.PxlrXHC", salt: "$2b$10$ShcZ4K9RhFoNIwaz7mt0re", userId: 2 }),
+      new UserPassword({ id: 3, value: "$2b$10$3KYZypHCPV3Z5gzZgnOivePQzYY8qXk8KgG3KQ5rO0nycx3yeQXRS", salt: "$2b$10$3KYZypHCPV3Z5gzZgnOive", userId: 3 }),
+      new UserPassword({ id: 4, value: "$2b$10$J2PwOvs5kfqKQHRj9VFkcu.q5GiQCbDSY2UNQq/5fOw0CvB/Y2q3O", salt: "$2b$10$J2PwOvs5kfqKQHRj9VFkcu", userId: 4 }),
+      new UserPassword({ id: 5, value: "$2b$10$/KK9JzMYxZNV3yMuA58Qd.XtrE8IlenhAKaTl/2YhYROPigU8yf.m", salt: "$2b$10$/KK9JzMYxZNV3yMuA58Qd.", userId: 5 })
     ]);
   }
 
