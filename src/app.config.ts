682cd44507e19cc430154967d927934b4aba0862
Adriano Cucci <cucciadriano@gmail.com>
2021-11-28 18:53:29 -0500

    Updated app.config.ts.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,18 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+const env: NodeJS.ProcessEnv = process.env;
+
 export default () => ({
-  port: Number(process.env.PORT),
-  jwtSecret: process.env.JWT_SECRET
+  httpPort: Number(env.HTTP_PORT),
+  jwtSecret: env.JWT_SECRET,
+  typeOrm: <TypeOrmModuleOptions> {
+    type: "postgres",
+    host: env.DB_HOST,
+    port: Number(env.DB_PORT),
+    database: env.DB_NAME,
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    synchronize: true
+  }
 });
