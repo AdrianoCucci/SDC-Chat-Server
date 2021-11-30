55877e6d020cf265aa1dffb6034613cf3d200cdd
Adriano Cucci <cucciadriano@gmail.com>
2021-11-30 16:31:10 -0500

    Fixed formatting on Organization entity model.

--- src/modules/core/organizations/entities/organization.entity.ts
+++ src/modules/core/organizations/entities/organization.entity.ts
@@ -13,25 +13,25 @@ export class Organization {
 
   @Column({ nullable: true })
   public email?: string;
- 
+
   @Column({ nullable: true })
   public phoneNumber?: string;
- 
+
   @Column({ nullable: true })
   public street?: string;
- 
+
   @Column({ nullable: true })
   public city?: string;
 
   @Column({ nullable: true })
   public province?: string;
-  
+
   @Column({ nullable: true })
   public country?: string;
-  
+
   @Column({ nullable: true })
   public postalCode?: string;
-  
+
 
   @OneToMany(() => User, entity => entity.organizationId)
   public users?: User[];
