143dc6ad1f71293f6d69cdf5a0ae4fec48e86360
Adriano Cucci <cucciadriano@gmail.com>
2021-12-04 23:49:27 -0500

    Updated ChatMessageQueryDto.

--- src/modules/core/chat-messages/dtos/chat-message-query.dto.ts
+++ src/modules/core/chat-messages/dtos/chat-message-query.dto.ts
@@ -1,35 +1,8 @@
-import { Type } from "class-transformer";
-import { IsDateString, IsInt, IsOptional, IsPositive } from "class-validator";
-import { ChatMessage } from "../entities/chat-message.entity";
+import { PartialType } from "@nestjs/mapped-types";
+import { IsDateString, IsOptional } from "class-validator";
+import { ChatMessageDto } from "./chat-message.dto";
 
-export class ChatMessageQuery {
-  public static getPredicate(query: ChatMessageQuery): (message: ChatMessage) => boolean {
-    let predicate: (message: ChatMessage) => boolean = null;
-
-    if(query != null) {
-      predicate = (message: ChatMessage) => {
-        const filters: boolean[] = [];
-
-        if(query.minDate != null) {
-          filters.push(new Date(message.datePosted) >= new Date(query.minDate));
-        }
-        if(query.maxDate != null) {
-          filters.push(new Date(message.datePosted) >= new Date(query.maxDate));
-        }
-        if(query.senderUserId != null) {
-          filters.push(message.senderUserId === query.senderUserId);
-        }
-        if(query.organizationId != null) {
-          filters.push(message.organizationId === query.organizationId);
-        }
-
-        return !filters.some((f: boolean) => f === false);
-      }
-    }
-
-    return predicate;
-  }
-
+export class ChatMessageQueryDto extends PartialType(ChatMessageDto) {
   @IsDateString()
   @IsOptional()
   public minDate?: Date | string;
@@ -37,16 +10,4 @@ export class ChatMessageQuery {
   @IsDateString()
   @IsOptional()
   public maxDate?: Date | string;
-
-  @Type(() => Number)
-  @IsInt()
-  @IsPositive()
-  @IsOptional()
-  public senderUserId?: number;
-
-  @Type(() => Number)
-  @IsInt()
-  @IsPositive()
-  @IsOptional()
-  public organizationId?: number;
 }
