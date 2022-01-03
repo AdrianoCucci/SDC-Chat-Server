132ed6805222c30210e6281a73824b7ab840785d
Adriano Cucci <cucciadriano@gmail.com>
2022-01-03 14:01:26 -0500

    Fixed minor bugs with ChatMessagesService.

--- src/modules/core/chat-messages/chat-messages.service.ts
+++ src/modules/core/chat-messages/chat-messages.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RepositoryBase } from 'src/utils/repository-base';
-import { FindConditions, FindManyOptions, FindOneOptions, LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
+import { DeepPartial, FindConditions, FindManyOptions, FindOneOptions, LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
 import { ChatMessageQueryDto } from './dtos/chat-message-query.dto';
 import { ChatMessage } from './entities/chat-message.entity';
 
@@ -11,14 +11,23 @@ export class ChatMessagesService extends RepositoryBase<ChatMessage> {
     super(repository);
   }
 
+  public getOneByModel(model: DeepPartial<ChatMessageQueryDto>): Promise<ChatMessage> {
+    return super.getOneByModel(model);
+  }
+
+  public getAllByModel(model: DeepPartial<ChatMessageQueryDto>): Promise<ChatMessage[]> {
+    return super.getAllByModel(model);
+  }
+
   protected onGettingByModel(model: ChatMessageQueryDto): FindOneOptions<ChatMessage> | FindManyOptions<ChatMessage> {
-    const findConditions: FindConditions<ChatMessage>[] = [model];
+    const { minDate, maxDate, ...rest } = model;
+    const findConditions: FindConditions<ChatMessage>[] = [rest];
 
     if(model.minDate) {
-      findConditions.push({ ...model, datePosted: MoreThanOrEqual(model.datePosted) });
+      findConditions.push({ ...rest, datePosted: MoreThanOrEqual(model.datePosted) });
     }
     if(model.maxDate) {
-      findConditions.push({ ...model, datePosted: LessThanOrEqual(model.datePosted) });
+      findConditions.push({ ...rest, datePosted: LessThanOrEqual(model.datePosted) });
     }
 
     return { where: findConditions };
