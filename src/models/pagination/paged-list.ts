310698e53d877d3b64f77d07707ae7cd7e928c6e
Adriano Cucci <cucciadriano@gmail.com>
2022-01-08 15:34:52 -0500

    Updated field iname in PagedList.

--- src/models/pagination/paged-list.ts
+++ src/models/pagination/paged-list.ts
@@ -3,13 +3,13 @@ import { PaginateMeta } from "./paginate-meta";
 
 export class PagedList<T = any> {
   public readonly data: T[];
-  public readonly meta: PaginateMeta;
+  public readonly pagination: PaginateMeta;
 
   public constructor(params: PagedListParams<T>) {
     this.data = params.data;
 
     const meta: PaginateMeta | PagedListCreateMeta = params.meta;
-    this.meta = meta instanceof PaginateMeta ? meta : new PaginateMeta(meta.pagination, params.data.length, meta.totalItemsCount);
+    this.pagination = meta instanceof PaginateMeta ? meta : new PaginateMeta(meta.pagination, params.data.length, meta.totalItemsCount);
   }
 }
 
